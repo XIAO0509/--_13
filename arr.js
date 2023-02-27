af3329126c22cf33070581f1b60dcacc85e306cd
XIAO0509 <liuxiaoxiao2089@gmail.com>
2023-02-27 22:44:48 +0300

    console

--- arr.js
+++ arr.js
@@ -1,4 +1,4 @@
 var str=prompt("Введите последовательность целых чисел","Введите ',' между числами");//Type string
 var arr=str.split(",");//Делить строки на массивvar arr1 = [new Set(arr)]; 
 var arr1 = [new Set(arr)]; 
-console.log(arr,arr1,arr1.length);
+console.log(str,arr,arr1,arr1.length);
